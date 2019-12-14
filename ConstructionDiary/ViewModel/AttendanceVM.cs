6b92a39e7efbac896f4fa092b58d057ed630b027
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2019-12-14 11:19:23 +0530

    db

---
+++ ConstructionDiary/ViewModel/AttendanceVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConstructionDiary
+{
+    public class AttendanceVM
+    {
+        public Guid AttendaceId { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public Guid ClientId { get; set; }
+        public decimal? TotalPaidAmount { get; set; }
+        public int TotalPerson { get; set; }
+        public int TotalFullDay { get; set; }
+        public int TotalHalfDay { get; set; }
+        public int TotalAbsent { get; set; }
+    }
+
+    public class PersonAttendanceVM
+    {
+        public Guid PersonAttendanceId { get; set; }
+        public Guid PersonId { get; set; }
+        public string PersonName { get; set; }
+        [Required(ErrorMessage = "Status is required.")]
+        public decimal AttendanceStatus { get; set; }
+        public Guid? SiteId { get; set; } 
+        public int? PersonDailyRate { get; set; }         
+    }
+
+    public class AttendanceFormVM
+    {
+        public Guid? AttendaceId { get; set; }
+        [Required]
+        public string AttendanceDate { get; set; }
+        public List<PersonAttendanceVM> lstPersonAttendance { get; set; }
+
+        public List<SelectListItem> SitesList { get; set; }
+        public List<SelectListItem> AttendanceStatusList { get; set; }
+    }
+
+    public class AttendanceStatusVM
+    {
+        public string StatusValue { get; set; }
+        public string StatusText { get; set; } 
+    }
+
+}
